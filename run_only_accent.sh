80d35c2daa0cd6eb40e6991e5d92db6f19e4c8e1
zjc6666 <36856753+zjc6666@users.noreply.github.com>
2021-04-13 17:22:33 +0800

    Update run_only_accent.sh

--- run_only_accent.sh
+++ run_only_accent.sh
@@ -7,9 +7,9 @@
 . ./cmd.sh || exit 1;
 
 
-cuda_cmd="slurm.pl --quiet --exclude=node0[2-7]"
-decode_cmd="slurm.pl --quiet --exclude=node0[1-4,8]"
-cmd="slurm.pl --quiet --exclude=node0[1-4]"
+cuda_cmd="slurm.pl --quiet --exclude=node0[3-5]"
+decode_cmd="slurm.pl --quiet --exclude=node0[1-4,9]"
+cmd="slurm.pl --quiet --exclude=node0[3-4]"
 # general configuration
 backend=pytorch
 steps=1
@@ -20,14 +20,12 @@ dumpdir=dump   # directory to dump full features
 N=0            # number of minibatches to be used (mainly for debugging). "0" uses all minibatches.
 verbose=0      # verbose option
 resume=        # Resume the training from snapshot
-log=100
 vocab_size=2000
 bpemode=bpe
 # feature configuration
 do_delta=false
 
 train_config=conf/espnet_train.yaml
-train_track1_config=conf/e2e_asr_transformer_only_accent.yaml
 lm_config=conf/espnet_lm.yaml
 decode_config=conf/espnet_decode.yaml
 preprocess_config=conf/espnet_specaug.yaml
@@ -40,14 +38,6 @@ lmtag=0             # tag for managing LMs
 recog_model=model.acc.best # set a model to be used for decoding: 'model.acc.best' or 'model.loss.best'
 n_average=5
 
-# others
-accum_grad=2
-n_iter_processes=2
-lsm_weight=0.0
-epochs=30
-elayers=6
-batch_size=32
-
 # exp tag
 tag="base" # tag for managing experiments.
 
@@ -58,7 +48,7 @@ set -e
 set -o pipefail
 
 . utils/parse_options.sh || exit 1;
-. path2.sh
+. path.sh
 
 steps=$(echo $steps | perl -e '$steps=<STDIN>;  $has_format = 0;
   if($steps =~ m:(\d+)\-$:g){$start = $1; $end = $start + 10; $has_format ++;}
@@ -79,15 +69,14 @@ fi
 data=$1 # data
 exp=$2 # exp-espnet-epoch-50
 train_set="train"
-recog_set="cv_all test"
 valid_set="valid"
-# recog_set="cv/UK cv/US cv/CHN cv/JPN cv/KR cv/RU cv/IND cv/PT"
+recog_set="cv_all test"
 
 if [ ! -z $step01 ]; then
    echo "extracting filter-bank features and cmvn"
-   for i in $recog_set $valid_set $train_set;do # $train_dev $recog_set;do
+    for i in $train_set $valid_set $recog_set;do 
       utils/fix_data_dir.sh $data/$i
-      steps/make_fbank_pitch.sh --cmd "$cmd" --nj $nj --write_utt2num_frames true \
+      steps/make_fbank.sh --cmd "$cmd" --nj $nj --write_utt2num_frames true \
           $data/$i $data/$i/feats/log $data/$i/feats/ark
       utils/fix_data_dir.sh $data/$i
    done
@@ -96,130 +85,164 @@ if [ ! -z $step01 ]; then
    echo "step01 Extracting filter-bank features and cmvn Done"
 fi
 
-### prepare for track1
+if [ ! -z $step02 ]; then
+   echo "generate label file and dump features for track2:E2E"
+
+   for x in ${train_set} ;do
+       dump.sh --cmd "$cmd" --nj $nj  --do_delta false \
+          $data/$x/feats.scp $data/${train_set}/cmvn.ark $data/$x/dump/log $data/$x/dump # for track2 e2e training
+   done
+
+   for x in ${valid_set} $recog_set;do 
+       dump.sh --cmd "$cmd" --nj $nj  --do_delta false \
+          $data/$x/feats.scp $data/${train_set}/cmvn.ark $data/$x/dump_${train_set}/log $data/$x/dump_${train_set} # for track2 e2e training
+   done
+   echo "step02 Generate label file and dump features for track2:E2E Done"   
+fi
+
+bpe_set=$train_set
+bpe_model=$data/lang/$train_set/${train_set}_${bpemode}_${vocab_size}
+dict=$data/lang/$train_set/${train_set}_${bpemode}_${vocab_size}_units.txt
+if [ ! -z $step03 ]; then
+   echo "stage 03: Dictionary Preparation" 
+
+   [ -d $data/lang/$train_set ] || mkdir -p $data/lang/$train_set || exit;
+   echo "<unk> 1" > ${dict}
+
+   awk '{$1=""; print}' $data/$bpe_set/text | sed -r 's#^ ##g' > $data/lang/$train_set/${train_set}_input.txt
+
+   spm_train --input=$data/lang/$train_set/${train_set}_input.txt --vocab_size=${vocab_size} --model_type=${bpemode} --model_prefix=${bpe_model} --input_sentence_size=100000000
+   spm_encode --model=${bpe_model}.model --output_format=piece < $data/lang/$train_set/${train_set}_input.txt | tr ' ' '\n' | sort | uniq | awk '{print $0 " " NR+1}' >> ${dict}
+   echo "stage 03: Dictionary Preparation Done"
+fi
+
+if [ ! -z $step04 ]; then
+    # make json labels
+    data2json.sh --nj $nj --cmd "${cmd}" --feat $data/${train_set}/dump/feats.scp --bpecode ${bpe_model}.model \
+       $data/${train_set} ${dict} > ${data}/${train_set}/${train_set}_${bpemode}_${vocab_size}.json
+
+    for i in test;do 
+       data2json.sh --nj 10 --cmd "${cmd}" --feat $data/$i/dump_${train_set}/feats.scp --bpecode ${bpe_model}.model \
+           $data/$i ${dict} > ${data}/$i/${train_set}_${bpemode}_${vocab_size}.json
+    done
+    echo "stage 04: Make Json Labels Done"
+fi
+
+
+lmexpdir=${exp}/${train_set}_rmmlm_${bpemode}
+# train rnnlm 
 if [ ! -z $step06 ]; then
-    for x in $train_set $valid_set $recog_set;do
-        awk '{printf "%s %s\n", $1, $1 }' $data/$x/text > $data/$x/spk2utt.utt
-        cp $data/$x/spk2utt.utt $data/$x/utt2spk.utt
-        compute-cmvn-stats --spk2utt=ark:$data/$x/spk2utt.utt scp:$data/$x/feats.scp \
-            ark,scp:$data/$x/cmvn_utt.ark,$data/$x/cmvn_utt.scp
-        local/tools/dump_spk_yzl23.sh --cmd "$cmd" --nj 20 \
-            $data/$x/feats.scp $data/$x/cmvn_utt.scp \
-            $data/$x/dump_utt/log $data/$x/dump_utt $data/$x/utt2spk.utt
-    done
-    echo "### step 06 dump utt Done"
-fi
-### prepare for track1
-if [ ! -z $step07 ]; then
-    for x in $recog_set $valid_set;do
-        local/tools/data2json.sh --nj 20 --cmd "$cmd" --feat $data/$x/dump_utt/feats.scp --text $data/$x/utt2accent --oov 8 $data/$x $data/lang/accent.dict > $data/$x/${train_set}_accent.json
-    done
-fi
-
-dict=$data/lang/accent.dict
-epochs=30
-if [ ! -z $step10 ]; then
-    train_set=train
-    elayers=3
-    expname=${train_set}_${elayers}_layers_verification_${backend}
-    expdir=$exp/${expname}
-    epoch_stage=0
-    mkdir -p ${expdir}
-    echo "stage 2: Network Training"
-    ngpu=1
-    if  [ ${epoch_stage} -gt 0 ]; then
-        echo "stage 6: Resume network from epoch ${epoch_stage}"
-        resume=${exp}/${expname}/results/snapshot.ep.${epoch_stage}
-    fi
-    train_track1_config=conf/track1_accent_transformer.yaml
-    ${cuda_cmd} --gpu ${ngpu} ${expdir}/train.log \
-        asr_train.py \
-        --config ${train_track1_config} \
-        --preprocess-conf ${preprocess_config} \
-        --ngpu ${ngpu} \
-        --backend ${backend} \
-        --outdir ${expdir}/results \
-        --debugmode ${debugmode} \
-        --debugdir ${expdir} \
-        --minibatches ${N} \
-        --verbose ${verbose} \
-        --resume ${resume} \
-        --report-interval-iters ${log} \
-        --accum-grad ${accum_grad} \
-        --n-iter-processes ${n_iter_processes} \
-        --elayers ${elayers} \
-        --lsm-weight ${lsm_weight} \
-        --epochs ${epochs} \
-        --batch-size ${batch_size} \
-        --dict ${dict} \
-        --num-save-ctc 0 \
-        --train-json $data/${train_set}/${train_set}_accent.json \
-        --valid-json $data/${valid_set}/${train_set}_accent.json
-fi
-
-# pretrained asr model
-pretrained_model=/home/maison2/lid/zjc/w2020/AESRC2020/result/track2-accent-160/train_12enc_6dec_pytorch/results/model.val5.avg.best
-if [ ! -z $step13 ]; then
-    train_set=train
-    elayers=12
-    expname=${train_set}_${elayers}_layers_init_libri_${backend}
-    expdir=$exp/${expname}
-    epoch_stage=0
-    mkdir -p ${expdir}
-    echo "stage 2: Network Training"
-    ngpu=1
-    if  [ ${epoch_stage} -gt 0 ]; then
-        echo "stage 6: Resume network from epoch ${epoch_stage}"
-        resume=${exp}/${expname}/results/snapshot.ep.${epoch_stage}
-    fi
-    ${cuda_cmd} --gpu ${ngpu} ${expdir}/train.log \
-        asr_train.py \
-        --config ${train_track1_config} \
-        --preprocess-conf ${preprocess_config} \
-        --ngpu ${ngpu} \
-        --backend ${backend} \
-        --outdir ${expdir}/results \
-        --debugmode ${debugmode} \
-        --debugdir ${expdir} \
-        --minibatches ${N} \
-        --verbose ${verbose} \
-        --resume ${resume} \
-        --report-interval-iters ${log} \
-        --accum-grad ${accum_grad} \
-        --n-iter-processes ${n_iter_processes} \
-        --elayers ${elayers} \
-        --lsm-weight ${lsm_weight} \
-        --epochs ${epochs} \
-        --batch-size ${batch_size} \
-        --dict ${dict} \
-        --num-save-ctc 0 \
-        --train-json $data/${train_set}/${train_set}_accent.json \
-        --valid-json $data/${train_valid}/${train_set}_accent.json \
-        ${pretrained_model:+--pretrained-model $pretrained_model}
-
-fi
-if [ ! -z $step15 ]; then
-    echo "stage 2: Decoding"
-    nj=100
-    for expname in train_3_layers_init_accent_verification_2_pytorch;do
-    for recog_set in test cv_all;do
-    decode_dir=decode_${recog_set}_${log_step}
-    use_valbest_average=true
-    expdir=$exp/$expname
     
-    if [[ $(get_yaml.py ${train_track1_config} model-module) = *transformer* ]]; then
+    lmdatadir=$exp/local/lm_${train_set}_${bpemode}
+    [ -d $lmdatadir ] ||  mkdir -p $lmdatadir
+    cut -f 2- -d" " $data/${train_set}/text | spm_encode --model=${bpe_model}.model --output_format=piece \
+        > ${lmdatadir}/${train_set}.txt
+    cut -f 2- -d" " $data/${train_valid}/text | spm_encode --model=${bpe_model}.model --output_format=piece \
+        > ${lmdatadir}/${train_valid}.txt
+ 
+    ${cuda_cmd} --gpu ${ngpu} ${lmexpdir}/train.log \
+        lm_train.py \
+        --config ${lm_config} \
+        --ngpu ${ngpu} \
+        --backend ${backend} \
+        --verbose 1 \
+        --outdir ${lmexpdir} \
+        --tensorboard-dir tensorboard/${lmexpname} \
+        --train-label ${lmdatadir}/${train_set}.txt \
+        --valid-label ${lmdatadir}/${train_valid}.txt \
+        --resume ${lm_resume} \
+        --dict ${dict}
+
+fi
+
+if [ ! -z $step07 ]; then
+    train_set=train
+    expname=${train_set}_12enc_6dec_verification_${backend}
+    expdir=$exp/${expname}
+    epoch_stage=0
+    mkdir -p ${expdir}
+    echo "stage 2: Network Training"
+    ngpu=1
+    if  [ ${epoch_stage} -gt 0 ]; then
+        echo "stage 6: Resume network from epoch ${epoch_stage}"
+        resume=${exp}/${expname}/results/snapshot.ep.${epoch_stage}
+    fi  
+    
+    ${cuda_cmd} --gpu $ngpu ${expdir}/train.log \
+         asr_train.py \
+                --config ${train_config} \
+                --preprocess-conf ${preprocess_config} \
+                --ngpu $ngpu \
+                --backend ${backend} \
+                --outdir ${expdir}/results \
+                --tensorboard-dir tensorboard/${expname} \
+                --debugmode ${debugmode} \
+                --dict ${dict} \
+                --debugdir ${expdir} \
+                --minibatches ${N} \
+                --verbose ${verbose} \
+                --resume ${resume} \
+                --train-json $data/${train_set}/${train_set}_${bpemode}_${vocab_size}.json \
+                --valid-json $data/${valid_set}/${train_set}_${bpemode}_${vocab_size}.json
+fi
+
+# pretraind model 
+pretrained_model=/home/maison2/lid/zjc/w2020/AESRC2020/result/librispeech960-asr/libspeech960_12enc_6dec_pytorch/results/model.val5.avg.best
+expdir=$exp/${expname}
+if [ ! -z $step08 ]; then
+    epoch_stage=0
+    train_set=train
+    expname=${train_set}_12enc_6dec_init_accent_and_libri_${backend}
+    expdir=$exp/${expname}
+    mkdir -p ${expdir}
+    echo "stage 2: Network Training"
+    ngpu=1
+    if  [ ${epoch_stage} -gt 0 ]; then
+        echo "stage 6: Resume network from epoch ${epoch_stage}"
+        resume=${exp}/${expname}/results/snapshot.ep.${epoch_stage}
+    fi
+    ${cuda_cmd} --gpu $ngpu ${expdir}/train.log \
+         asr_train.py \
+                --config ${train_config} \
+                --preprocess-conf ${preprocess_config} \
+                --ngpu $ngpu \
+                --backend ${backend} \
+                --outdir ${expdir}/results \
+                --tensorboard-dir tensorboard/${expname} \
+                --debugmode ${debugmode} \
+                --dict ${old_dict} \
+                --debugdir ${expdir} \
+                --minibatches ${N} \
+                --verbose ${verbose} \
+                --resume ${resume} \
+                --train-json $data/${train_set}/${train_set}_${bpemode}_${vocab_size}.json \
+                --valid-json $data/${valid_set}/${train_set}_${bpemode}_${vocab_size}.json \
+                --n-iter-processes $ngpu \
+                --dec-init $pretrained_model \
+                --dec-init-mods='decoder.decoders' \
+                --enc-init $pretrained_model \
+                --enc-init-mods='encoder.'
+fi
+
+if [ ! -z $step09 ]; then
+    echo "stage 3: Decoding"
+    nj=100
+    for expname in train_12enc_6dec_verification_pytorch;do
+    expdir=$exp/${expname}
+    for recog_set in test cv_all;do
+    echo "#### ${expname}"
+    use_valbest_average=true
+    if [[ $(get_yaml.py ${train_config} model-module) = *transformer* ]]; then
         # Average ASR models
         if ${use_valbest_average}; then
             [ -f ${expdir}/results/model.val5.avg.best ] && rm ${expdir}/results/model.val5.avg.best
-            recog_model=model.val${n_average}_${log_step}.avg.best
+            recog_model=model.val${n_average}.avg.best
             opt="--log ${expdir}/results/log"
         else
             [ -f ${expdir}/results/model.last5.avg.best ] && rm ${expdir}/results/model.last5.avg.best
             recog_model=model.last${n_average}.avg.best
             opt="--log"
         fi
-        # recog_model=model.acc.best
-        echo "$recog_model"
         average_checkpoints.py \
             ${opt} \
             --backend ${backend} \
@@ -227,25 +250,37 @@ if [ ! -z $step15 ]; then
             --out ${expdir}/results/${recog_model} \
             --num ${n_average}
     fi
-    # split data
-    dev_root=$data/${recog_set}
-    splitjson.py --parts ${nj} ${dev_root}/${train_set}_accent.json
-    #### use CPU for decoding
-    ngpu=0
 
-    ${decode_cmd} JOB=1:${nj} ${expdir}/${decode_dir}/log/decode.JOB.log \
-        asr_recog.py \
-        --ngpu ${ngpu} \
-        --backend ${backend} \
-        --batchsize 0 \
-        --recog-json ${dev_root}/split${nj}utt/${train_set}_accent.JOB.json \
-        --result-label ${expdir}/${decode_dir}/${train_set}_accent.JOB.json \
-        --model ${expdir}/results/${recog_model} 
-    concatjson.py ${expdir}/${decode_dir}/${train_set}_accent.*.json >  ${expdir}/${decode_dir}/${train_set}_accent.json
-    python local/tools/parse_track1_jsons.py  ${expdir}/${decode_dir}/${train_set}_accent.json ${expdir}/${decode_dir}/result.txt
-    python local/tools/parse_track1_jsons.py  ${expdir}/${decode_dir}/${train_set}_accent.json ${expdir}/${decode_dir}/result.txt > ${expdir}/${decode_dir}/acc.txt
+    pids=() # initialize pids
+    for rtask in ${recog_set}; do
+    (
+        decode_dir=decode_${rtask}_$(basename ${decode_config%.*})
+        feat_recog_dir=$data/$rtask
+	echo $feat_recog_dir 
+        # split data
+        splitjson.py --parts ${nj} ${feat_recog_dir}/${train_set}_${bpemode}_${vocab_size}.json
+        #### use CPU for decoding
+        ngpu=0
+
+        ${decode_cmd} JOB=1:${nj} ${expdir}/${decode_dir}/log/decode.JOB.log \
+            asr_recog.py \
+            --config ${decode_config} \
+            --ngpu ${ngpu} \
+            --backend ${backend} \
+            --batchsize 0 \
+            --recog-json ${feat_recog_dir}/split${nj}utt/${train_set}_${bpemode}_${vocab_size}.JOB.json \
+            --result-label ${expdir}/${decode_dir}/data.JOB.json \
+            --model ${expdir}/results/${recog_model} #\
+            # --rnnlm ${lmexpdir}/rnnlm.model.best
+        score_sclite.sh --bpe ${vocab_size} --bpemodel ${bpe_model}.model --wer true ${expdir}/${decode_dir} ${old_dict} 
+    )&
+    pids+=($!) # store background pids
+    done
+    i=0; for pid in "${pids[@]}"; do wait ${pid} || ((++i)); done
+    [ ${i} -gt 0 ] && echo "$0: ${i} background jobs are failed." && false
     done
     done
-    echo "Decoding finished"
+    # done
+    echo "Finished"
 fi
 
