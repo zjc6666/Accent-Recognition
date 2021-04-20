8347add2f12d398079febedbd641157a3d49777f
zjc6666 <36856753+zjc6666@users.noreply.github.com>
2021-04-20 15:31:48 +0800

    Update add-codec.sh

--- add-codec/add-codec.sh
+++ add-codec/add-codec.sh
@@ -36,12 +36,8 @@ if [ ! -z "$steps" ]; then
   declare step$index=1
   done
 fi
+data_set=train
 source_dir=/home/maison2/lid/zjc/w2021/add-codecs
-des_dir=/home/maison2/lid/zjc/w2021/add-codecs
-
-sdata=$source_dir/librispeech960
-data_8k=$source_dir/librispeech960_8k
-data_codec=$source_dir/data/librispeech960_codec
 
 sample_rate=8000
 codec_set="train"
@@ -49,7 +45,7 @@ codec_set="train"
 
 if [ ! -z $step01 ]; then
   echo -e "\n ##LOG [step 1] down-sample to 8k data start @ `date`\n"
-  for x in librispeech960 test;do
+  for x in ${train_set};do
     utils/data/copy_data_dir.sh $source_dir/$x $source_dir/${x}_8k
     utils/data/resample_data_dir.sh $sample_rate $source_dir/${x}_8k
   done
@@ -57,12 +53,11 @@ if [ ! -z $step01 ]; then
 fi
 
 # codec
-
 if [ ! -z $step02 ]; then
-    for x in librispeech960;do
+    for x in ${train_set};do
         utils/data/get_utt2dur.sh $source_dir/$x
         data_8k=$source_dir/${x}_8k
-        data_codec=$des_dir/${x}_codec
+        data_codec=$source_dir/${x}_codec
         echo -e "\n ##LOG [step 2] codec data start @ `date`\n"
         codec_list=./codec-list_full.txt
         ./scripts/copy-data-by-adding-codec.sh  --cmd "run.pl"  --steps 1,2,3 $data_8k $codec_list $data_codec
